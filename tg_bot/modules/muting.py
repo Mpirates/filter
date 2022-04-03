d78548ffcfc454e11660f2d13b76d0b4d65a311e
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-04-03 11:59:12 +0530

    Update muting.py

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -177,7 +177,7 @@ def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
     return ""
 
 
-__help__ = """
+__GMC__ = """
 *Admin only:*
  - /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
  - /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
