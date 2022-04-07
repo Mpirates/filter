0a5d394d0486ef79a09b8287e8fe0daf40e16855
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-04-07 12:24:38 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -57,17 +57,17 @@ USER_SETTINGS = {}
 GDPR = []
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("tg_bot.modules." + module_name)
-    if not hasattr(imported_module, "__mod_name__"):
-        imported_module.__mod_name__ = imported_module.__name__
+    imported_module = importlib.import_module("tg_bot.modules.misc.py" + module_name)
+    if not hasattr(imported_module, "__misc__"):
+        imported_module.__misc__ = imported_module.__name__
 
-    if not imported_module.__mod_name__.lower() in IMPORTED:
-        IMPORTED[imported_module.__mod_name__.lower()] = imported_module
+    if not imported_module.__misc__.lower() in IMPORTED:
+        IMPORTED[imported_module.__misc__.lower()] = imported_module
     else:
         raise Exception("Can't have two modules with the same name! Please change one")
 
     if hasattr(imported_module, "__help__") and imported_module.__help__:
-        HELPABLE[imported_module.__mod_name__.lower()] = imported_module
+        HELPABLE[imported_module.__misc__.lower()] = imported_module
 
     # Chats to migrate on chat_migrated events
     if hasattr(imported_module, "__migrate__"):
